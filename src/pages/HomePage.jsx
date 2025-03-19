39af84ad18fc5d0c395f318e875f877ad4199547
Mitchell Lagaras <mookielagaras@gmail.com>
2025-03-19 06:37:14 -0400

    tidied up more boilerplate and added background color to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from 'react';
+
 const HomePage = () => {
+  const bgColor = "#465e85";
+  useEffect(() => {
+    document.body.style.backgroundColor = bgColor;
+    document.body.style.margin = "0px";
+    return () => {
+      document.body.style.backgroundColor = "";
+      document.body.style.margin = "";
+    }
+  }, [])
+
   return (
   <div>
     HomePage 
